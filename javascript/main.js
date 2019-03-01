79692eeae0fcf5b35f8e851659d9049430f38b05
bryleewilliamson <47310248+bryleewilliamson@users.noreply.github.com>
2019-03-01 21:12:15 +0800

    modeling pages

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  /* filtering */
+  $('#portfolio-grid').masonry({
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 200,
+  gutter: 10,
+  stagger: 30
+  });
+
+
+});
